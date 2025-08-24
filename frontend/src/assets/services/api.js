897d169304f10b073551856185db5634f96436d5
0x000alek <aolguin.or@gmail.com>
2025-08-23 22:21:39 -0400

    chore(hito 4): reemplazar url base localhost por url backend render

--- frontend/src/assets/services/api.js
+++ frontend/src/assets/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api/v1"
+    baseURL: "https://wawita-api.onrender.com/api/v1"
 })
 
 API.interceptors.request.use(
