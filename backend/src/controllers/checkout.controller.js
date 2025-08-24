02da6aba83cd5caf8df8e13e366305da6f982b8d
0x000alek <aolguin.or@gmail.com>
2025-08-23 21:02:29 -0400

    refactor(hito 4): aplicar reglas de prettier

--- backend/src/controllers/checkout.controller.js
+++ backend/src/controllers/checkout.controller.js
@@ -1,17 +1,14 @@
 const checkout = async (req, res) => {
-    try {
-        return res.json({
-            message: 'Checkout successful',
-            cart: req.body,
-            user: req.user
-        })
-    } catch (error) {
-        console.error(
-            'Internal server error occurred during checkout:',
-            error
-        );
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
+  try {
+    return res.json({
+      message: 'Checkout successful',
+      cart: req.body,
+      user: req.user,
+    });
+  } catch (error) {
+    console.error('Internal server error occurred during checkout:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
-export default { checkout }
+export default { checkout };
